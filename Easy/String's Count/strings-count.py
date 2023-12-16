44017b559348742eef72c659028203352651f543
Muskan Gautam <152010462+muskangautam64@users.noreply.github.com>
2023-12-16 19:24:09 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/String's Count/strings-count.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+
+    def countStr(self, n):
+        # code here
+        result = (n - 1) * n + 2 * n + 1 + (n * (n - 1) / 2) + (n * (n - 1) * (n - 2) / 2)
+        return int(result)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        n = int(input())
+
+        solObj = Solution()
+
+        print(solObj.countStr(n))
+# } Driver Code Ends
