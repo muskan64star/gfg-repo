a499dbc746c9021956ee45d749a2217c9672644b
Muskan Gautam <152010462+muskangautam64@users.noreply.github.com>
2023-12-19 22:41:01 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Rightmost different bit/rightmost-different-bit.py
@@ -0,0 +1,57 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+
+    
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the first position with different bits.
+    def posOfRightMostDiffBit(self,m,n):
+        #Your code here
+        if m==n:
+            return -1
+        ans = m^n
+        pos = 1
+        while ans%2!=1:
+            pos+=1
+            ans//=2
+        
+        return pos
+
+#{ 
+ # Driver Code Starts.
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        mn=[int(x) for x in input().strip().split()]
+        m=mn[0]
+        n=mn[1]
+        ob=Solution()
+        print(math.floor(ob.posOfRightMostDiffBit(m,n)))
+        
+        
+        
+        
+        T-=1
+    
+    
+
+
+
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
