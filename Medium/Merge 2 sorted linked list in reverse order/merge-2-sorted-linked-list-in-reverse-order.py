f825825806581249d6eedfa72ccd739c385e9e71
Muskan Gautam <152010462+muskangautam64@users.noreply.github.com>
2024-01-08 21:01:25 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge 2 sorted linked list in reverse order/merge-2-sorted-linked-list-in-reverse-order.py
@@ -0,0 +1,87 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+'''
+
+class Solution:
+    def mergeResult(self, h1, h2):
+        val = []
+        while h1:
+            val.append(h1.data)
+            h1 = h1.next
+        
+        while h2:
+            val.append(h2.data)
+            h2 = h2.next
+            
+        val.sort(reverse = True)
+        node = Node(0)
+        temp = node
+        for i in val:
+            n = Node(i)
+            temp.next = n
+            temp = temp.next
+        return node.next
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class Llist:
+    def __init__(self):
+        self.head=None
+    
+    def insert(self,data,tail):
+        node=Node(data)
+        
+        if not self.head:
+            self.head=node
+            return node
+        
+        tail.next=node
+        return node
+        
+
+def printList(head):
+    while head:
+        print(head.data,end=' ')
+        head=head.next
+        
+if __name__ == '__main__':
+    t=int(input())
+    
+    for tcs in range(t):
+        
+        n1,n2=[int(x) for x in input().split()]
+        
+        arr1=[int(x) for x in input().split()]
+        ll1=Llist()
+        tail=None
+        for nodeData in arr1:
+            tail=ll1.insert(nodeData,tail)
+            
+            
+        arr2=[int(x) for x in input().split()]
+        ll2=Llist()
+        tail=None
+        for nodeData in arr2:
+            tail=ll2.insert(nodeData,tail)
+        
+        
+        ob = Solution()
+        resHead=ob.mergeResult(ll1.head,ll2.head)
+        printList(resHead)
+        print()
+        
+    
+    
+# } Driver Code Ends
