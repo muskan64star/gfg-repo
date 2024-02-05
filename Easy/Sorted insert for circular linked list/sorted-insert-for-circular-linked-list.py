a98a3d055f5efc92421013e6dd814959f4c2b199
Muskan Gautam <152010462+muskangautam64@users.noreply.github.com>
2024-02-05 13:17:31 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sorted insert for circular linked list/sorted-insert-for-circular-linked-list.py
@@ -0,0 +1,91 @@
+#User function Template for python3
+
+'''
+class Node: 
+    # Constructor to initialize the node object 
+    def __init__(self, data): 
+        self.data = data 
+        self.next = None
+  '''
+class Solution:
+    def sortedInsert(self, head, data):
+        newnode=Node(data)
+        if not head:
+            newnode.next=newnode
+            return newnode
+        curr=head
+        if data<head.data:
+            while curr.next!=head:
+                curr=curr.next
+            curr.next=newnode
+            newnode.next=head
+            return newnode
+            
+        curr=head
+        while curr.next!=head and curr.next.data<data:
+            curr=curr.next
+        newnode.next=curr.next
+        curr.next=newnode
+        return head
+
+#{ 
+ # Driver Code Starts
+#contributed by RavinderSinghPB
+class Node: 
+    # Constructor to initialize the node object 
+    def __init__(self, data): 
+        self.data = data 
+        self.next = None
+  
+class LinkedList: 
+  
+    # Function to initialize head 
+    def __init__(self): 
+        self.head = None
+        self.last=None
+  
+    # Function to insert a new node  
+    def push(self, data):
+        if not self.head:
+            nn=Node(data)
+            self.head=nn
+            self.last=nn
+            nn.next=nn
+            return
+        nn=Node(data)
+        nn.next=self.head
+        self.last.next=nn
+        self.last=nn 
+  
+
+# Utility function to print the linked LinkedList
+
+def printList(head):
+    if not head:
+        return
+    temp = head 
+    print (temp.data,end=' ') 
+    temp = temp.next
+    while(temp != head): 
+        print (temp.data,end=' ') 
+        temp = temp.next
+  
+    
+if __name__ =='__main__':
+    t=int(input())
+    for tcs in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().split()]
+        data=int(input())
+
+        cll=LinkedList()
+        for e in arr:
+            cll.push(e)
+            
+        reshead=Solution().sortedInsert(cll.head,data)
+        printList(reshead)
+        print()
+        
+
+
+# } Driver Code Ends
