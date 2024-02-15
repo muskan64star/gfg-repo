55cd2281cfc0b0c5e434f2a02a868d46daf41368
Muskan Gautam <152010462+muskangautam64@users.noreply.github.com>
2024-02-15 09:36:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count all Possible Path/count-all-possible-path.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def isPossible(self, paths):
+        return int(all(sum(r)%2 == 0 for r in paths))
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		paths = []
+		for _ in range(n):
+			cur = list(map(int, input().split()))
+			paths.append(cur)
+		obj = Solution()
+		ans = obj.isPossible(paths)
+		print(ans)
+
+# } Driver Code Ends
