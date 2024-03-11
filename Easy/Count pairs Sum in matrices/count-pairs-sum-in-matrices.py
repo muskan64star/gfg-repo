1e477c2b15c758b16e9cf1e0a1a08474192f5315
Muskan Gautam <152010462+muskangautam64@users.noreply.github.com>
2024-03-11 23:37:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+class Solution:
+	def countPairs(self, mat1, mat2, n, x):
+		a=[j for i in mat1 for j in i]
+		b=[j for i in mat2 for j in i]
+		b=set(b)
+		ans=0
+		for i in a:
+		    if x-i in b:
+		        ans+=1
+		return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n , x = input().split()
+		n,x = int(n), int(x)
+		mat1 = []
+		for _ in range(n):
+			a = [int(x) for x in input().split()]
+			mat1.append(a)
+
+		mat2 = []
+		for _ in range(n):
+			a = [int(x) for x in input().split()]
+			mat2.append(a)
+
+		ob = Solution()
+		ans = ob.countPairs(mat1, mat2, n, x)
+		print(ans)
+
+# } Driver Code Ends
