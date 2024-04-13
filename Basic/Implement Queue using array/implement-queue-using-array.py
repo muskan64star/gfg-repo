fafdf0fc97779c048d081617bf5de65fb0c8fc84
Muskan Gautam <152010462+muskangautam64@users.noreply.github.com>
2024-04-13 16:31:29 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Implement Queue using array/implement-queue-using-array.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+from collections import deque
+class MyQueue:
+    def __init__(self):
+        self.arr = deque()
+    
+    #Function to push an element x in a queue.
+    def push(self, x):
+        #add code here
+        self.arr.append(x)
+        return self.arr
+     
+    #Function to pop an element from queue and return that element.
+    def pop(self): 
+        #add code here
+        if len(self.arr)==0:
+            return -1
+        return self.arr.popleft()
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        s=MyQueue()
+        q=int(input())
+        q1=list(map(int,input().split()))
+        i=0
+        while(i<len(q1)):
+            if(q1[i]==1):
+                s.push(q1[i+1])
+                i=i+2
+            elif(q1[i]==2):
+                print(s.pop(),end=" ")
+                i=i+1
+            elif(s.isEmpty()):
+                print(-1)
+                i=i+1
+        print()   
+
+# } Driver Code Ends
