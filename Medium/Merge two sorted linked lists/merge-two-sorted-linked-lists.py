dc22ed5579cccec65e678693e3e64362535cd54a
Muskan Gautam <152010462+muskangautam64@users.noreply.github.com>
2024-04-14 22:32:42 +0530

    Updated Solution - GfG to GitHub

--- Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.py
+++ Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.py
@@ -29,14 +29,10 @@ def sortedMerge(head1, head2):
             ptr.next = head2
             head2 = head2.next
         ptr = ptr.next
-    while head1:
+    if head1:
         ptr.next = head1
-        head1 = head1.next
-        ptr = ptr.next
-    while head2:
+    if head2:
         ptr.next = head2
-        head2 = head2.next
-        ptr = ptr.next
     return ans.next     
 
 
